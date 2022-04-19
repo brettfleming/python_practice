56753e25c13a8077e6e426b0137d7e90b02c7699
Brett Fleming <brettfleming015@gmail.com>
2022-04-18 19:48:08 -0500

    using print and letting the computer talk

--- script.py
+++ script.py
@@ -3,3 +3,7 @@ print("Hello and welcome " + my_name + "!")
 
 #This is my first python comment lets Go!!!
 #to comment in python you use the # symbol instead of the // like in javascript
+
+
+# in python you use print instead of console.log() from javascript
+print("Hello World!")
