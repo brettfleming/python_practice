25630266688f0d6c2c366245a6978018f93f7696
Brett Fleming <brettfleming015@gmail.com>
2022-04-18 19:43:06 -0500

    comments

--- script.py
+++ script.py
@@ -1,2 +1,5 @@
-my_name = "Codecademy"
+my_name = "Brett"
 print("Hello and welcome " + my_name + "!")
+
+#This is my first python comment lets Go!!!
+#to comment in python you use the # symbol instead of the // like in javascript
