8c65298a40f8ea4ec2b01e5a4b9298a357230951
Brett Fleming <brettfleming015@gmail.com>
2022-04-18 19:54:52 -0500

    learning varibles and their syntacs

--- script.py
+++ script.py
@@ -9,3 +9,24 @@ print("Hello and welcome " + my_name + "!")
 print("Hello World!")
 # strings can be made with either "" or ''
 print('Brett')
+
+#variables dont need to be declared with the word const or let like in javascript
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal =  "sandwhich"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = 'pasta'
+# Printing out dinner
+print("Dinner:")
+print(meal)
