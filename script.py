fc12e34ff9975ced689ffbe71d8120c1a4d6cfd7
Brett Fleming <brettfleming015@gmail.com>
2022-04-20 19:19:36 -0500

    working with the modulo operator

--- script.py
+++ script.py
@@ -81,3 +81,15 @@ print(7 ** 2)
 print(8 ** 2)
 # How many squares for 6 people to have 6 quilts each that are 6x6?
 print((6 ** 2) ** 2)
+
+# the modulo operator just gives you the remander of a divsion calculation
+my_team = 27 % 4 # remander of 3
+
+
+print(my_team)
+
+person1 = 26 % 4 # remander of 2
+person2 = 28 % 4 # remander of 0 ( this means it evenly divids into the number)
+
+print(person1)
+print(person2)
