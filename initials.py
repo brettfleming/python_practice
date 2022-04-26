6cb7d538081875fccf30feddc3e239f2cdde24d5
Brett Fleming <brettfleming015@gmail.com>
2022-04-25 20:32:12 -0500

    starting a small project

--- initials.py
+++ initials.py
@@ -1,10 +1,10 @@
 #Brett Fleming
 #I played college soccer
 
-print('BBBB ')
-print('B   B')
-print('B   B')
-print('BBB  ')
-print('B   B')
-print('B   B')
-print('BBBB ')
+print('BBBB   FFFFF')
+print('B   B  F    ')
+print('B   B  F    ')
+print('BBB    FFFFF ')
+print('B   B  F    ')
+print('B   B  F    ')
+print('BBBB   F    ')
