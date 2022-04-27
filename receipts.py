5d9bea12ea1254ca5f2de71cc19ec7b268637101
Brett Fleming <brettfleming015@gmail.com>
2022-04-27 15:20:23 -0500

    commenting on my code and reveiwing it

--- receipts.py
+++ receipts.py
@@ -1,3 +1,5 @@
+#the point of this prject is to display the customers receipt to the console
+
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 lovely_loveseat_price = 254.00
 
