74186eafc36b3a5638a383e3a4d0d9fcc04e527b
Brett Fleming <brettfleming015@gmail.com>
2022-04-26 20:30:14 -0500

    starting another small prject

--- receipts.py
+++ receipts.py
@@ -1,5 +1,4 @@
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
-
 lovely_loveseat_price = 254.00
 
 stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
@@ -20,5 +19,12 @@ customer_one_total += lovely_loveseat_price
 
 customer_one_itemization += lovely_loveseat_description
 
+customer_one_total += luxurious_lamp_price
+
+customer_one_itemization += luxurious_lamp_description
+
+customer_one_tax = customer_one_total * sales_tax
+
+customer_one_total += customer_one_tax
 
 
