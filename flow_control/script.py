a327693d9e283e33613bd2d2074f0f6d47590c88
Brett Fleming <brettfleming015@gmail.com>
2022-05-02 20:17:25 -0500

    working with comparators

--- flow_control/script.py
+++ flow_control/script.py
@@ -7,3 +7,11 @@ statement_two = "Yes"
 statement_three = "No"
 
 statement_four = "No"
+
+
+#(5 * 2) - 1 == 8 + 1
+statement_one = True
+#13 - 6 != (3 * 2) + 1
+statement_two = False
+#3 * (2 - 1) == 4 - 1
+statement_three = True
