07b50c024830e1b2ac286ae8c5a17ac26e653aa8
Brett Fleming <brettfleming015@gmail.com>
2022-05-02 20:23:51 -0500

    working with comparators

--- flow_control/script.py
+++ flow_control/script.py
@@ -15,3 +15,13 @@ statement_one = True
 statement_two = False
 #3 * (2 - 1) == 4 - 1
 statement_three = True
+
+#not a boolean value
+#must be capitalized and not in qoutes
+my_baby_bool = 'true'
+
+print(type(my_baby_bool)) #returns str 
+#this is a boolean
+my_baby_bool_two = True
+
+print(type(my_baby_bool_two)) #returns bool 
