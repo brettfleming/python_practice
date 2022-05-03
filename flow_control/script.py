83ecab759b0bd24bf0f48ee44e25d4fb763738b3
Brett Fleming <brettfleming015@gmail.com>
2022-05-02 20:33:55 -0500

    working with if statements

--- flow_control/script.py
+++ flow_control/script.py
@@ -25,3 +25,13 @@ print(type(my_baby_bool)) #returns str
 my_baby_bool_two = True
 
 print(type(my_baby_bool_two)) #returns bool 
+
+
+# Enter a user name here, make sure to make it a string
+user_name = 'angela_catlady_87'
+
+if user_name == "Dave":
+  print("Get off my computer Dave!")
+
+if user_name == "angela_catlady_87":
+  print("I know it is you, Dave! Go away!")
