6856b99faaebc31c37657a172ca4291509c7f5fd
Brett Fleming <brettfleming015@gmail.com>
2022-05-03 20:33:16 -0500

    working with elif statements

--- flow_control/script.py
+++ flow_control/script.py
@@ -96,3 +96,17 @@ if (credits >= 120) and (gpa >= 2.0):
   print("You meet the requirements to graduate!")
 else:
   print("You do not meet the requirements to graduate.")
+
+
+grade = 86
+
+if grade >= 90:
+  print('A')
+elif grade >= 80:
+  print('B')
+elif grade >= 70:
+  print('C')
+elif grade >= 60:
+  print('D')
+else:
+  print('F')
