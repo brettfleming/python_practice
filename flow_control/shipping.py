f1abb912b13fc88df549f513ae73b4c9433bb83b
Brett Fleming <brettfleming015@gmail.com>
2022-05-05 19:55:00 -0500

    workin on a  new small project

--- flow_control/shipping.py
+++ flow_control/shipping.py
@@ -1,4 +1,4 @@
-weight = 8.4
+weight = 1.5
 premium_ground = 125
 #Ground Shipping
 if weight <= 2:
@@ -18,6 +18,23 @@ else:
   print('cost of regular ground shipping:')
   print(cost)
 
-  
+
 print('cost of premium ground shipping:')
 print(premium_ground)
+
+if weight <= 2:
+  cost = weight * 4.5
+  print('cost of drone shipping:')
+  print(cost)
+elif weight <= 6:
+  cost = weight * 9
+  print('cost of drone shipping:')
+  print(cost)
+elif weight <= 10:
+  cost = weight * 12
+  print('cost of drone shipping:')
+  print(cost)
+else:
+  cost = weight * 14.25
+  print('cost of drone shipping:')
+  print(cost)
