84f39e0683086ad52f7256e1defcda28ea380c6c
Brett Fleming <brettfleming015@gmail.com>
2022-05-05 19:47:39 -0500

    workin on a  new small project

--- flow_control/shipping.py
+++ flow_control/shipping.py
@@ -1,10 +1,14 @@
-weight = 4
+weight = 5
 #Ground Shipping
 if weight <= 2:
-  print()
+  cost = weight * 1.5 + 20
+  print(cost)
 elif weight <= 6:
-  print()
+  cost = weight * 3 + 20
+  print(cost)
 elif weight <= 10:
-  print()
+  cost = weight * 4 + 20
+  print(cost)
 else:
-  print()
+  cost = weight * 4.75 + 20 
+  print(cost)
