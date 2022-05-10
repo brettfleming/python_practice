a3872c4b5cbc3c9a0bc3b3fa9fd65af55e6035fb
Brett Fleming <brettfleming015@gmail.com>
2022-05-09 19:55:47 -0500

    looking at errors in python

--- flow_control/errors.py
+++ flow_control/errors.py
@@ -25,3 +25,13 @@ score = 0
 option1 = 'Fresca'
 option2 = 'V8'
 option4 = 'A&W'
+
+
+print("For ordering his favorite beverages on demand, LBJ had four buttons installed in the Oval Office labeled 'Coffee', 'Tea', 'Coke', and what?\n")
+
+print("A.", option1)
+print("B.", option2)
+print("C.", option3)
+print("D.", option4)
+  
+answer = 'a'
