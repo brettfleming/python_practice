a33ef90f2c8e2b20483ae38412489380653a2417
Brett Fleming <brettfleming015@gmail.com>
2022-05-16 19:31:13 -0500

    reviewing the lesson

--- flow_control/errors.py
+++ flow_control/errors.py
@@ -67,3 +67,12 @@ radius = 36
 area = math.pi * radius * radius
     
 print("The circle area is", area)
+
+# key take aways
+
+#SyntaxError: Error caused by not following the proper structure (syntax) of the language.
+
+#NameError: Errors reported when the interpreter detects an object that is unknown.
+
+#TypeError: Errors thrown when an operation is applied to an object of an inappropriate type.
+
