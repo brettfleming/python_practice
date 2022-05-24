ce066807398a0de390300fa0922c70a32a57bcf8
Brett Fleming <brettfleming015@gmail.com>
2022-05-23 19:32:55 -0500

    cont lists

--- lists/script.py
+++ lists/script.py
@@ -7,3 +7,6 @@ broken_heights = [65, 71, 59, 62] #adding the commas is what fixes the errors
 ints_and_strings = [1, 2, 3, "four", "five", "six"]
 
 sam_height_and_testscore = ["Sam", 67, 85.5, True]
+
+#lists can be empty to start 
+my_empty_list = []
