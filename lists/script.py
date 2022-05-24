37c8bbc0c9996643567ae286884071e2f2804055
Brett Fleming <brettfleming015@gmail.com>
2022-05-23 19:42:30 -0500

    starting list methods

--- lists/script.py
+++ lists/script.py
@@ -31,3 +31,15 @@ orders.append('tulips')
 
 orders.append('roses')
 #print(orders)
+
+
+
+orders = ["daisy", "buttercup", "snapdragon", "gardenia", "lily"]
+new_orders = ['lilac', 'iris']
+
+orders_combined = orders + new_orders
+
+#broken_prices = [5, 3, 4, 5, 4] + 4
+broken_prices = [5, 3, 4, 5, 4] + [4] #adding in []'s is all you need to do to fix the new list
+
+
