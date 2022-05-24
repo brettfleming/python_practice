1c470260d3aaa331baec9475e65bfe8150284b9f
Brett Fleming <brettfleming015@gmail.com>
2022-05-23 19:31:58 -0500

    cont lists

--- lists/script.py
+++ lists/script.py
@@ -3,4 +3,7 @@ heights = [61, 70, 67, 64, 65]
 #broken_heights = [65 71 59 62]
 broken_heights = [65, 71, 59, 62] #adding the commas is what fixes the errors
 
-ints_and_strings = [1, 2, 3, "four", "five"]
+#in python lists can have multipule data types in them at once
+ints_and_strings = [1, 2, 3, "four", "five", "six"]
+
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
