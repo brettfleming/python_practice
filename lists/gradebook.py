568fa7802ce9d97e1649897940a67df0fd595201
Brett Fleming <brettfleming015@gmail.com>
2022-05-25 19:26:00 -0500

    starting a small porject

--- lists/gradebook.py
+++ lists/gradebook.py
@@ -7,7 +7,8 @@ grades = [98, 97, 85, 88]
 gradebook = [['physics', 98], ['calculus', 97], ['poetry', 85], ['history', 88]]
 print(gradebook)
 
-
+gradebook.append(['computer science', 100])
+#print(gradebook)
 
 
 
