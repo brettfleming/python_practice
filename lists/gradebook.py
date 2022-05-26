f84b2761dbb6fb73ac27fc825a0ea331a482d4a5
Brett Fleming <brettfleming015@gmail.com>
2022-05-25 19:24:25 -0500

    starting a small porject

--- lists/gradebook.py
+++ lists/gradebook.py
@@ -4,6 +4,9 @@ last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["arc
 subjects = ['physics', 'calculus', 'poetry', 'history']
 grades = [98, 97, 85, 88]
 
+gradebook = [['physics', 98], ['calculus', 97], ['poetry', 85], ['history', 88]]
+print(gradebook)
+
 
 
 
